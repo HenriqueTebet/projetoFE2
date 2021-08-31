fc83fb64837e417c670b47abd53899a384f71bd2
Henrique <henrique.empresa53@outlook.com>
2021-08-31 17:33:23 -0300

    Adicionando arquivos iniciais do projeto

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+
+
+let pegarValor = function(){
+    document.querySelector("input");
+    let urlImagem = document.getElementById("url");
+     return urlImagem
+}
+
+let button = document.getElementById("enviar");
+
+
+button.addEventListener("click",function(event){
+    event.preventDefault();
+    let url = pegarValor();
+    
+    
+});
+
