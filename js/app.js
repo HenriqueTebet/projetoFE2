ff7086e072eee1e5e065e82353d969305067a9e8
Samuel Xavier <xavierfsamuel@gmail.com>
2021-09-01 15:57:04 -0300

    Adicionando funcionalidades no Js parte1

--- js/app.js
+++ js/app.js
@@ -3,17 +3,22 @@
 
 let pegarValor = function(){
     document.querySelector("input");
-    let urlImagem = document.getElementById("url");
-     return urlImagem
+    let urlImagem = document.getElementById("url").value;
+    // urlImagem.setAttribute("src","urlImagem")
+    return urlImagem
 }
 
 let button = document.getElementById("enviar");
 
 
+// button.setAttribute("")
+
+
 button.addEventListener("click",function(event){
     event.preventDefault();
-    let url = pegarValor();
-    
-    
+    let imge = document.querySelector("img");
+    imge.setAttribute("src",`${pegarValor()}}`);
+    // let url = pegarValor();
+    console.log(imge);
 });
 
