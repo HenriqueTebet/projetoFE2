98faa08a6273d963d69e8972de7dc8eaf3ffa446
Samuel Xavier <xavierfsamuel@gmail.com>
2021-09-02 18:27:59 -0300

    Adicionando Estilizaçâo do CSS e validação dos campos no JS

--- js/app.js
+++ js/app.js
@@ -10,10 +10,36 @@ let pegarValor = function(){
 let button = document.getElementById("enviar");
 
 
+// criando um verificação para os tres campos de input
+let verificarCampos = document.getElementsByName("Verificar");
+
+//Função para validação parcial 
+function validarCampos (){
+    if(verificarCampos[0].value == ""){
+    alert('Por favor, preencha o todos os nome');
+    // document.getElementsByName("Verificar").focus();
+    }
+    else if(verificarCampos[1].value == ""){
+    alert('Por favor, preencha o todos os descricao');
+    // document.getElementsByName("Verificar").focus();
+    }
+    else if(verificarCampos[2].value == ""){
+    alert('Por favor, preencha o todos os url');
+    // document.getElementsByName("Verificar").focus();
+    }
+
+}
+
+
+
 /************ Adicionamos uma função ao button com base em um evento *************/
 button.addEventListener("click",function(event){
     event.preventDefault();
     let img = document.querySelector("img");
     img.setAttribute("src",`${pegarValor()}`);
-});
+
+    validarCampos();
+})
+
+
 
