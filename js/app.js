aa2dd11594946d88701f28c3f75416ee24ec8cb5
Samuel Xavier <xavierfsamuel@gmail.com>
2021-09-08 15:36:53 -0300

    adicionando metodo trim

--- js/app.js
+++ js/app.js
@@ -13,21 +13,24 @@ let button = document.getElementById("enviar");
 // criando um verificação para os tres campos de input
 let verificarCampos = document.getElementsByName("Verificar");
 
+
 //Função para validação parcial 
 function validarCampos (){
+    
     if(verificarCampos[0].value == ""){
-    alert('Por favor, preencha o todos os nome');
-    // document.getElementsByName("Verificar").focus();
+        alert('Por favor, preencha o campo nome');
+        // document.getElementsByName("Verificar").focus();
     }
     else if(verificarCampos[1].value == ""){
-    alert('Por favor, preencha o todos os descricao');
+        alert('Por favor, preencha  o descricao');
     // document.getElementsByName("Verificar").focus();
     }
     else if(verificarCampos[2].value == ""){
-    alert('Por favor, preencha o todos os url');
+    alert('Por favor, preencha a url');
     // document.getElementsByName("Verificar").focus();
     }
 
+    
 }
 
 
@@ -37,8 +40,11 @@ button.addEventListener("click",function(event){
     event.preventDefault();
     let img = document.querySelector("img");
     img.setAttribute("src",`${pegarValor()}`);
-
+    
     validarCampos();
+  
+    document.getElementById("titulo-carro").innerHTML += titulo.value.trim();
+  
 })
 
 
