c25f58e6794a394ecf4445726a09aab82b287498
Samuel Xavier <xavierfsamuel@gmail.com>
2021-09-10 17:33:32 -0300

    Finalização de ajustes

--- js/app.js
+++ js/app.js
@@ -8,53 +8,94 @@ let button = document.getElementById("enviar");
 let verificarCampos = document.getElementsByName("Verificar");
 
 //Função para validação parcial 
-function validarCampos (){
-    
-    if(verificarCampos[0].value == ""){
+function validarCampos() {
+
+    if (verificarCampos[0].value == "") {
         alert('Por favor, preencha o campo nome');
-        // document.getElementsByName("Verificar").focus();
+        return
+
     }
-    else if(verificarCampos[1].value == ""){
+    else if (verificarCampos[1].value == "") {
         alert('Por favor, preencha  o descricao');
-    // document.getElementsByName("Verificar").focus();
+        return
+        
     }
-    else if(verificarCampos[2].value == ""){
-    alert('Por favor, preencha a url');
-    // document.getElementsByName("Verificar").focus();
+    else if (verificarCampos[2].value == "") {
+        alert('Por favor, preencha a url');
+        return
+        
     }
 
-    
+    criarCard();
+
 }
 
 let titulo = "";
 let descricao = "";
 let urlImagem = "";
+let element = 0;
+let json = {
+    'id': [],
+    'imagemJson': [],
+    'tituloJson': [],
+    'descricaoJson': []
+}
 
 
-function criarCard(){
+function criarCard() {
     let titulo = document.getElementById("titulo").value;
     let descricao = document.getElementById("descricao").value;
     let urlImagem = document.getElementById("url").value;
-    
+
     let card = document.getElementById("container-cards");
-    
+
     card.innerHTML += `<div class="cards"> <img src= "${urlImagem}">
     <h2 id="titulo-carro">${titulo}</h2>
     <p id="descricaoP" >${descricao}</p> </div>`;
-    
-    
+
+
+
+    json.imagemJson.push(urlImagem);
+    json.tituloJson.push(titulo);
+    json.descricaoJson.push(descricao);
+
+    json = JSON.stringify(json);
+
+
+    localStorage.setItem("json", json);
+    json = JSON.parse(json);
+
 }
 
+
 /************ Adicionamos uma função ao button com base em um evento *************/
-button.addEventListener("click",function(event){
+button.addEventListener("click", function (event) {
     event.preventDefault();
-    // let img = document.querySelector("img");
-    // img.setAttribute("src",`${pegarValor()}`);
-    
-    
-    criarCard();
+
     validarCampos();
 
 })
 
+
+
+
+window.onload = function () {
+
+    let array = localStorage.getItem("json");
+    array = JSON.parse(array);
+
+
+    for (element in array.imagemJson) {
+
+        let card = document.getElementById("container-cards");
+        card.innerHTML += `<div class="cards"> <img src= "${array.imagemJson[element]}">
+            <h2 id="titulo-carro">${array.tituloJson[element]}</h2>
+            <p id="descricaoP" >${array.descricaoJson[element]}</p> </div>`;
+
+        json.imagemJson.push(array.imagemJson[element]);
+        json.tituloJson.push(array.tituloJson[element]);
+        json.descricaoJson.push(array.descricaoJson[element]);
+    }
+
+}
 
